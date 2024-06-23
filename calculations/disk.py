fix units in shows

calculations/disk.py
@@ -1,8 +1,8 @@
 import sys
 import numpy as np
 import pandas as pd
-from scipy import interpolate, integrate
-import matplotlib.pyplot as plt
+from scipy import interpolate
+import matplotlib as mpl, matplotlib.pyplot as plt
 
 from material import Material
 
@@ -42,11 +42,15 @@ class Dick:
         assert all(map(lambda i: i >= 0, rholes))
         assert all(map(lambda i: i >= 0, dholes))
 
-        assert all(radius[i] < radius[i + 1] for i in range(len(radius) - 1))
+        assert all(radius[i] < radius[i + 1] for i in range(len(radius) - 1))  # проверка сортировки по возрастанию
+
+        # расстояние между краями отв по окружности
+        self.b = np.array([2 * np.pi * rholes[i] / nholes[i] - dholes[i] for i in range(len(nholes))])
+        assert all(map(lambda i: i > 0, self.b))
 
         self.radius, self.thickness = np.array(radius), np.array(thickness)
         self.material = material
-        self.nholes = nholes
+        self.nholes, self.rholes, self.dholes = np.array(nholes), np.array(rholes), np.array(dholes)
 
     # TODO: numpy vectorizaoin
     def __calculation1(self, rotation_frequency: int | float, pressure0: int | float,
@@ -99,9 +103,8 @@ class Dick:
 
         return {'tension_t': sigma_t, 'tension_r': sigma_r}
 
-    @decorators.timeit(4)
     def tension(self, rotation_frequency: float, temperature0: int | float,
-                pressure: tuple | list, temperature: tuple | list) -> dict[str: tuple | list | np.ndarray]:
+                pressure: tuple | list, temperature: tuple | list) -> dict[str:  np.ndarray]:
         """Метод двух расчетов"""
 
         average_radius = np.array([(self.radius[i] + self.radius[i + 1]) / 2 for i in range(len(self.radius) - 1)])
@@ -124,7 +127,6 @@ class Dick:
         avarege_E = np.array([self.material.E(av_t) for av_t in avarege_temperature])
         avarege_mu = np.array([self.material.mu(av_t) for av_t in avarege_temperature])
 
-        # TODO: multiprocessing
         calc1 = self.__calculation1(rotation_frequency, pressure[0],
                                     self.radius, self.thickness, self.tetta,
                                     avarege_density, avarege_E, avarege_mu)
@@ -136,78 +138,113 @@ class Dick:
                 sigma_t[i][j] = calc1['tension_t'][i][j] + k * calc2['tension_t'][i][j]
                 sigma_r[i][j] = calc1['tension_r'][i][j] + k * calc2['tension_r'][i][j]
 
-        av_sigma_t = np.array([sigma_t[0][0]] +
-                              [(sigma_t[i][1] + sigma_t[i + 1][0]) / 2 for i in range(0, len(self.radius) - 2)] +
-                              [sigma_t[-1][-1]])
-        av_sigma_r = np.array([sigma_r[0][0]] +
-                              [(sigma_r[i][1] + sigma_r[i + 1][0]) / 2 for i in range(0, len(self.radius) - 2)] +
-                              [sigma_r[-1][-1]])
-        sigma = np.array([np.sqrt(av_sigma_t[i] ** 2 - av_sigma_t[i] * av_sigma_r[i] + av_sigma_r[i] ** 2)
+        sigma_t = np.array([sigma_t[0][0]] +
+                           [(sigma_t[i][1] + sigma_t[i + 1][0]) / 2 for i in range(0, len(self.radius) - 2)] +
+                           [sigma_t[-1][-1]])
+        sigma_r = np.array([sigma_r[0][0]] +
+                           [(sigma_r[i][1] + sigma_r[i + 1][0]) / 2 for i in range(0, len(self.radius) - 2)] +
+                           [sigma_r[-1][-1]])
+        sigma = np.array([np.sqrt(sigma_t[i] ** 2 - sigma_t[i] * sigma_r[i] + sigma_r[i] ** 2)
                           for i in range(len(radius))])
 
-        tensions = {'tension': sigma, 'tension_t': av_sigma_t, 'tension_r': av_sigma_r}
+        result = {'radius': self.radius, 'thickness': self.thickness,
+                  'tension': sigma, 'tension_t': sigma_t, 'tension_r': sigma_r}
 
-        self.show_tension(tensions)
+        df = pd.DataFrame({'radius [mm]': result['radius'] * 1000,
+                           'thickness [mm]': result['thickness'] * 1000,
+                           'tension [MPa]': result['tension'] / 10 ** 6,
+                           'tension_t': result['tension_t'] / 10 ** 6,
+                           'tension_r': result['tension_r'] / 10 ** 6}).sort_values(by='radius [mm]', ascending=False)
+        print(df)
 
-        return tensions
+        f_sigma_t = interpolate.interp1d(result['radius'], result['tension_t'], kind='linear')
+        f_sigma_r = interpolate.interp1d(result['radius'], result['tension_r'], kind='linear')
+
+        for i in range(len(self.nholes)):
+            print()
+            print(f'holes: {i}')
+            print(
+                f'nholes []: {self.nholes[i]}, rholes [mm]: {self.rholes[i] * 1000}, dholes [mm]: {self.dholes[i] * 1000}')
+            k = 3 - self.dholes[i] / self.b[i] - f_sigma_r(self.rholes[i]) / f_sigma_t(self.rholes[i])
+            sigma_t_hole = k * f_sigma_t(self.rholes[i]) / 10 ** 6
+            print(f'tension_t [MPa] in {(sigma_t_hole * 1.1, sigma_t_hole * 1.15)}')
+
+        self.show_tension(result)
+
+        return result
 
     def show_tension(self, tensions, **kwargs) -> None:
+
+        radius, thickness = self.radius * 1_000, self.thickness * 1_000  # приведение к [мм]
+        for key in tensions:
+            if key.startswith('tension'):
+                tensions[key] = tensions[key] / 10 ** 6  # приведение к [МПа]
+
+        k = 1.2
+        l = max(radius)
+        h = l * k
+        ylim = 0 - (h - l) / 2, max(radius) + (h - l) / 2
+
         fg = plt.figure(figsize=kwargs.pop('figsize', (16, 8)))
         gs = fg.add_gridspec(1, 2)  # строки, столбцы
 
         fg.add_subplot(gs[0, 0])
         plt.title("Disk", fontsize=14, fontweight='bold')
 
-        plt.plot([-self.thickness[0] / 1.5, self.thickness[0] / 1.5], [0, 0],
+        plt.plot([-thickness[0] / 1.5, thickness[0] / 1.5], [0, 0],
                  color='orange', linestyle='dashdot', linewidth=1.5)  # ось вращения
-        plt.plot(self.thickness / 2, self.radius, -self.thickness / 2, self.radius,
+        plt.plot(thickness / 2, radius, -thickness / 2, radius,
                  color='black', linestyle='solid', linewidth=3)
-        plt.plot([-self.thickness[-1] / 2, self.thickness[-1] / 2], [self.radius[-1], self.radius[-1]],
+        plt.plot([-thickness[-1] / 2, thickness[-1] / 2], [radius[-1], radius[-1]],
                  color='black', linestyle='solid', linewidth=3)  # периферия
-        if self.radius[0] > 0:
-            plt.plot([-self.thickness[0] / 2, self.thickness[0] / 2], [self.radius[0], self.radius[0]],
+        if radius[0] > 0:
+            plt.plot([-thickness[0] / 2, thickness[0] / 2], [radius[0]] * 2,
                      color='black', linestyle='solid', linewidth=3)  # втулка
-        # TODO: добавить прямоугольники сечений
+        for i in range(len(radius) - 1):  # сечения
+            av_th = (thickness[i] + thickness[i + 1]) / 2
+            plt.gca().add_patch(mpl.patches.Rectangle((-av_th / 2, radius[i]),
+                                                      av_th, radius[i + 1] - radius[i],
+                                                      angle=0, rotation_point='xy', alpha=0.5))
+
+        # TODO: добавить оси направлений r и t
         plt.grid(True)
         plt.axis('equal')
-        plt.xlabel("Thickness", fontsize=12)
-        plt.ylabel("Radius", fontsize=12)
+        plt.ylim(ylim)
+        plt.xlabel("Thickness [mm]", fontsize=12)
+        plt.ylabel("Radius [mm]", fontsize=12)
 
         fg.add_subplot(gs[0, 1])
         plt.title('Tension', fontsize=14, fontweight='bold')
 
-        plt.plot(tensions['tension'] / 10 ** 6, self.radius * 1000, label='экв',
-                 color='black', linestyle='solid', linewidth=3)
-        plt.plot(tensions['tension_t'] / 10 ** 6, self.radius * 1000, label='t',
-                 color='red', linestyle='solid', linewidth=2)
-        plt.plot(tensions['tension_r'] / 10 ** 6, self.radius * 1000, label='r',
-                 color='blue', linestyle='solid', linewidth=2)
-
-        plt.plot(tensions['tension'] / 10 ** 6, self.radius * 1000, )
+        plt.plot(tensions['tension'], radius, label='equivalent', color='black', linestyle='solid', linewidth=3)
+        plt.plot(tensions['tension_t'], radius, label='tangential', color='red', linestyle='solid', linewidth=2)
+        plt.plot(tensions['tension_r'], radius, label='radial', color='blue', linestyle='solid', linewidth=2)
 
         plt.legend()
         plt.grid(True)
+        plt.ylim(ylim)
         plt.xlabel("Tension [MPa]", fontsize=12)
         plt.ylabel("Radius [mm]", fontsize=12)
 
         plt.show()
 
     def show(self, **kwargs) -> None:
+        radius, thickness = self.radius * 1_000, self.thickness * 1_000  # приведение к [мм]
         plt.figure(figsize=kwargs.pop('figsize', (8, 8)))
         plt.title("Disk", fontsize=14, fontweight='bold')
-        plt.plot([-self.thickness[0] / 1.5, self.thickness[0] / 1.5], [0, 0],
+        plt.plot([-thickness[0] / 1.5, thickness[0] / 1.5], [0, 0],
                  color='orange', linestyle='dashdot', linewidth=1.5)  # ось вращения
-        plt.plot(self.thickness / 2, self.radius, -self.thickness / 2, self.radius,
+        plt.plot(thickness / 2, radius, -thickness / 2, radius,
                  color='black', linestyle='solid', linewidth=3)
-        plt.plot([-self.thickness[-1] / 2, self.thickness[-1] / 2], [self.radius[-1], self.radius[-1]],
+        plt.plot([-thickness[-1] / 2, thickness[-1] / 2], [radius[-1], radius[-1]],
                  color='black', linestyle='solid', linewidth=3)  # периферия
-        if self.radius[0] > 0:
-            plt.plot([-self.thickness[0] / 2, self.thickness[0] / 2], [self.radius[0], self.radius[0]],
+        if radius[0] > 0:
+            plt.plot([-thickness[0] / 2, thickness[0] / 2], [radius[0], radius[0]],
                      color='black', linestyle='solid', linewidth=3)  # втулка
         plt.grid(True)
         plt.axis('equal')
-        plt.xlabel("Thickness", fontsize=12)
-        plt.ylabel("Radius", fontsize=12)
+        plt.xlabel("Thickness [mm]", fontsize=12)
+        plt.ylabel("Radius [mm]", fontsize=12)
 
         plt.show()
 
@@ -231,8 +268,8 @@ if __name__ == "__main__":
                         })
 
     disk = Dick(radius=radius, thickness=thickness,
-                material=material)
+                material=material, nholes=[5], rholes=[66.8 / 1000], dholes=[6.2 / 1000])
 
-    # disk.show()
-    print(disk.tension(rotation_frequency=2806.2, temperature0=293.15,
-                       pressure=(0, 120.6 * 10 ** 6), temperature=(350, 650)))
+    disk.show()
+    disk.tension(rotation_frequency=2806.2, temperature0=293.15,
+                 pressure=(0, 120.6 * 10 ** 6), temperature=(350, 650))