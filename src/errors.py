2246c34e1452f839d8a102413f36eb06864e6c06
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-09-15 19:31:07 +0300

    fix errors

--- src/errors.py
+++ src/errors.py
@@ -1,3 +1,7 @@
 # шаблоны типичных ошибок
-SUBSTANCE_ATTRIBUTE_ERROR = "{} has not attribute {}"
+SUBSTANCE_ATTRIBUTE_ERROR = "'{}' has not attribute '{}'"
 ITERATION_LIMIT_ERROR = "Iteration limit in {}"
+
+NEGATIVE_TEMPERATURE_ERROR = "Absolute temperature '{}' < 0 K"
+
+EFFICIENCY_ERROR = "Efficiency {} not in [0..1]"
