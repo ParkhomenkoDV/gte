05bd4d393ea1dafd47cc774ef493c90103c13c8d
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-09-23 17:59:25 +0300

    add integral_average

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
-from mathematics import integral_average
+import numpy as np
+from scipy.integrate import nquad
 
 
 def call_with_kwargs(function, kwargs: dict):
@@ -15,28 +16,52 @@ def call_with_kwargs(function, kwargs: dict):
     return function(**arguments)
 
 
-def integral_average_fix(function, *ranges):
-    """Среднеинтегральное значение при одинаковых границах интегрирования"""
+def integral_average(function, **kwargs) -> tuple[float, float]:
+    """Среднее интегральное"""
     assert callable(function), TypeError(f"{function} must be callable")
 
-    def wrapper(*ranges):
-        return function(*[])
-
-
-def integral_average_kwargs(function, kwargs: dict):
-    """"""
-    assert callable(function), TypeError(f"{function} must be callable")
-    assert isinstance(kwargs, dict), TypeError(f"type {kwargs} must be dict")
-
-    ranges = []
-    for arg in function.__code__.co_varnames:
-        range_ = kwargs.get(arg)
-        assert range_ is not None, ValueError(f"value {range_} must be not None")
-        assert isinstance(range_, (tuple, list)), TypeError(
-            f"type range must be tuple, not {type(range_)}"
+    partial_args, other_args, devider = {}, {}, 1.0
+    arg_names = function.__code__.co_varnames[: function.__code__.co_argcount]
+    for arg_name in arg_names:
+        rang = kwargs.get(arg_name)
+        assert rang is not None, Exception(
+            f"function {function} require argument {arg_name}"
         )
-        ranges.append(range_)
+        assert isinstance(rang, (tuple, list, np.ndarray)), TypeError(
+            f"type of range must be tuple, but has {type(rang)}"
+        )
+        assert len(rang) == 2, ValueError(f"integral has 2 ranges, but has {len(rang)}")
 
-    function_ = integral_average_fix(function, *ranges)
+        if rang[0] == rang[1]:
+            partial_args[arg_name] = rang[0]
+        else:
+            other_args[arg_name] = (rang[0], rang[1])
+            devider *= rang[1] - rang[0]
 
-    return integral_average(function_, *ranges)
+    def partial(*args):
+        all_args = partial_args.copy()
+
+        for arg_name in arg_names:
+            # Если этот аргумент не фиксирован И еще не заполнен
+            if arg_name not in partial_args and arg_name not in all_args:
+                all_args[arg_name] = args[len(all_args) - len(partial_args)]
+
+        # Сортируем аргументы по порядку в функции
+        sorted_args = [all_args[arg] for arg in arg_names]
+        return function(*sorted_args)
+
+    if not other_args:
+        return function(**partial_args), 0.0
+
+    ranges = tuple(other_args.values())
+    result, abserr = nquad(partial, ranges)
+
+    return result / devider, abserr
+
+
+if __name__ == "__main__":
+
+    def test_func(temperature, y):
+        return temperature * y
+
+    print(integral_average(test_func, temperature=(300, 600), y=(1, 1)))
