b7022c26429e0dc67ca4d4bb7381475e5985b129
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-11-02 01:02:36 +0300

    add check_pressure_ratio

--- gte/checks.py
+++ gte/checks.py
@@ -6,3 +6,8 @@ def check_efficiency(efficiency: float) -> bool:
 def check_temperature(temperature: float) -> bool:
     """Проверка на отрицательную абсолютную температуру"""
     return 0 <= temperature
+
+
+def check_pressure_ratio(pressure_ratio: float) -> bool:
+    """Проверка степени повышения/понижения давления"""
+    return 1 <= pressure_ratio
