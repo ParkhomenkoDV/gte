c5f36cb94a1c77b03d7706f40e386e9460580c15
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-11-03 22:57:19 +0300

    fix tests

--- gte/nodes_test.py
+++ gte/nodes_test.py
@@ -13,14 +13,16 @@ from turbine import Turbine
 
 
 class TestNode:
-    @pytest.mark.parametrize("Node", [Compressor, CombustionChamber])
+    """Тесты абстрактного класса GTENode"""
+
+    @pytest.mark.parametrize("Node", [Compressor, CombustionChamber, Turbine])
     def test_init(self, Node):
         node = Node()
         assert isinstance(node, Node)
         assert isinstance(node.name, str)
         assert getattr(node, "leak") == 0
 
-    @pytest.mark.parametrize("Node", [Compressor, CombustionChamber])
+    @pytest.mark.parametrize("Node", [Compressor, CombustionChamber, Turbine])
     def test_name(self, Node):
         node = Node()
         assert node.name == Node.__name__
@@ -28,6 +30,13 @@ class TestNode:
         assert node.name == "C"
         node = Node("123")
         assert node.name == "123"
+
+    @pytest.mark.parametrize("Node", [Compressor, CombustionChamber, Turbine])
+    def test_leak(self, Node):
+        node = Node()
+        assert node.leak == 0
+        node.leak = 0.5
+        assert node.leak == 0.5
 
 
 @pytest.fixture
@@ -46,6 +55,15 @@ class TestCompressor:
         assert isnan(getattr(compressor, gtep.effeff))
         assert isnan(getattr(compressor, gtep.power))
         assert compressor.leak == 0.0
+
+    @pytest.mark.benchmark
+    def test_bench_init(self, benchmark):
+        """Бенчмарк инициализации компрессора"""
+
+        def benchfunc():
+            return Compressor()
+
+        benchmark(benchfunc)
 
     @pytest.mark.parametrize(
         "pipi, effeff, power, expected_count",
@@ -102,6 +120,27 @@ class TestCompressor:
             assert getattr(compressor, expected.key) == pytest.approx(expected.value, rel=EPSREL)
 
     @pytest.mark.parametrize(
+        "node, kwargs",
+        [
+            (Compressor(), {gtep.pipi: 6.0, gtep.effeff: 0.85}),
+            (Compressor(), {gtep.effeff: 0.85, gtep.power: 24 * 10**6}),
+            (Compressor(), {gtep.pipi: 6.0, gtep.power: 24 * 10**6}),
+        ],
+    )
+    @pytest.mark.benchmark
+    def test_bench_calculate(self, benchmark, node, kwargs):
+        def benchfunc(node, kwargs):
+            for k, v in kwargs.items():
+                setattr(node, k, v)
+
+            node.calculate(air)
+
+            for k in node.variables:
+                setattr(node, k, np.nan)
+
+        benchmark(benchfunc, node, kwargs)
+
+    @pytest.mark.parametrize(
         "effeff, TT, pipi, expected",
         [
             (0.75, 300, 6, True),  # нормальный КПД
@@ -147,6 +186,15 @@ class TestCombustionChamber:
         assert isnan(getattr(cc, gtep.peff))
         assert cc.leak == 0.0
 
+    @pytest.mark.benchmark
+    def test_bench_init(self, benchmark):
+        """Бенчмарк инициализации камеры сгорания"""
+
+        def benchfunc():
+            return CombustionChamber()
+
+        benchmark(benchfunc)
+
     @pytest.mark.parametrize(
         "peff, efficiency_burn, leak, error, expected",
         [
@@ -178,6 +226,25 @@ class TestCombustionChamber:
             for k, v in expected.items():
                 assert outlet.parameters[k] == pytest.approx(v, rel=EPSREL)
 
+    @pytest.mark.parametrize(
+        "node, kwargs",
+        [
+            (CombustionChamber(), {gtep.peff: 0.95, "efficiency_burn": 0.99}),
+        ],
+    )
+    @pytest.mark.benchmark
+    def test_bench_calculate(self, benchmark, node, kwargs):
+        def benchfunc(node, kwargs):
+            for k, v in kwargs.items():
+                setattr(node, k, v)
+
+            node.calculate(air, kerosene)
+
+            for k in node.variables:
+                setattr(node, k, np.nan)
+
+        benchmark(benchfunc, node, kwargs)
+
 
 @pytest.fixture
 def turbine():
@@ -195,6 +262,15 @@ class TestTurbine:
         assert isnan(getattr(turbine, gtep.effeff))
         assert isnan(getattr(turbine, gtep.power))
         assert turbine.leak == 0.0
+
+    @pytest.mark.benchmark
+    def test_bench_init(self, benchmark):
+        """Бенчмарк инициализации компрессора"""
+
+        def benchfunc():
+            return Turbine()
+
+        benchmark(benchfunc)
 
     expected = namedtuple("expected", ["key", "value"])
 
@@ -230,6 +306,27 @@ class TestTurbine:
             assert gtep.PP in outlet.parameters
 
             assert getattr(turbine, expected.key) == pytest.approx(expected.value, rel=EPSREL)
+
+    @pytest.mark.parametrize(
+        "node, kwargs",
+        [
+            (Turbine(), {gtep.pipi: 4.0, gtep.effeff: 0.9}),
+            (Turbine(), {gtep.effeff: 0.9, gtep.power: 24 * 10**6}),
+            (Turbine(), {gtep.pipi: 4.0, gtep.power: 24 * 10**6}),
+        ],
+    )
+    @pytest.mark.benchmark
+    def test_bench_calculate(self, benchmark, node, kwargs):
+        def benchfunc(node, kwargs):
+            for k, v in kwargs.items():
+                setattr(node, k, v)
+
+            node.calculate(exhaust)
+
+            for k in node.variables:
+                setattr(node, k, np.nan)
+
+        benchmark(benchfunc, node, kwargs)
 
 
 if __name__ == "__main__":
