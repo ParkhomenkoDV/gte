84f40baa934734d2e4a2c559ac93909b612aad74
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-11-19 09:35:04 +0300

    remove GTE from __init__

--- gte/__init__.py
+++ gte/__init__.py
@@ -1,10 +1,17 @@
 from .combustion_chambler import CombustionChamber
 from .compressor import Compressor
 from .config import parameters
-from .gte import GTE
+
+# from .gte import GTE
 from .turbine import Turbine
 
 # from src.nodes.outlet import Outlet
 
 # import *
-__all__ = ["parameters", "GTE", "Compressor", "CombustionChamber", "Turbine"]
+__all__ = [
+    "parameters",
+    # "GTE",
+    "Compressor",
+    "CombustionChamber",
+    "Turbine",
+]
