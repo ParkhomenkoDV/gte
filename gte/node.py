9a837bc9f0ed663299be2970f6c66c6b93ab05a0
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-12-31 23:11:52 +0300

    add class TurboCompressor

--- gte/node.py
+++ gte/node.py
@@ -1,18 +1,18 @@
 from abc import ABC, abstractmethod
-from itertools import product
-from typing import Any, Dict, Iterable, Tuple
+from typing import Any, Dict, Tuple
 
-import numpy as np
-from numpy import isnan, nan
 from substance import Substance
+from thermodynamics import adiabatic_index, сritical_sonic_velocity
 from thermodynamics import parameters as tdp
 
 try:  # Попытка относительного импорта
     from .config import parameters as gtep
     from .errors import SUBSTANCE_ATTRIBUTE_ERROR
+    from .utils import call_with_kwargs
 except ImportError:  # Резервный абсолютный импорт
     from config import parameters as gtep
     from errors import SUBSTANCE_ATTRIBUTE_ERROR
+    from utils import call_with_kwargs
 
 """
 Порядок расчета ТД параметров:
@@ -25,12 +25,11 @@ class GTENode(ABC):
 
     models: Dict[str, Any] = {}  # ML модели
 
-    __slots__ = ["name", "inlet", "outlet", "leak"]  # list
+    __slots__ = ["name", "characteristic"]  # list to add
 
     def __init__(self, name: str = "node") -> None:
-        assert isinstance(name, str), TypeError(f"type name must be str, but has {type(name)}")
+        assert isinstance(name, str), TypeError(f"{type(name)=} must be str")
         self.name: str = name
-        self.leak: float = 0
 
     def __str__(self) -> str:
         return self.name
@@ -38,60 +37,19 @@ class GTENode(ABC):
     def __delattr__(self, name: str) -> None:
         if name == "name":
             self.name = self.__class__.__name__
-        elif name == "inlet":
-            self.inlet = Substance("inlet")
-        elif name == "outlet":
-            self.outlet = Substance("outlet")
+        elif name == "characteristic":
+            raise Exception("deleting characteristic is prohibited!")
         else:
             return super().__delattr__(name)
 
     @classmethod
-    def generator(cls, **variables) -> Iterable[Any]:
-        """
-        Генератор объектов узлов ГТД
-
-        Генератор, который принимает параметры в **variables,
-        где ключ - атрибут класса, а значение - список значений данного атрибута.
-
-        Возвращает объект узла ГТД с установленными параметрами, перебирая все возможные комбинации.
-
-        Args:
-            **variables: Параметры вида attribute_name=[value1, value2, ...]
-
-        Yields:
-            Объекты класса с установленными параметрами
-        """
-        if not variables:  # словарь для перебора пустой
-            yield cls()
-            return
-
-        names, values = [], []
-        for variable, value in variables.items():  # валидация переданных параметров
-            assert isinstance(value, (tuple, list, np.ndarray)), TypeError(f"{type(value)=} must be tuple")
-            names.append(variable)
-            values.append(value)
-
-        for combination in product(*values):
-            obj = cls()
-            print(obj.variables)
-
-            for k, v in zip(names, combination):
-                setattr(obj, k, v)  # установка значений атрибутов
-
-            yield obj
-
-    @property
-    @abstractmethod
-    def variables(self) -> Dict[str, float]:
-        """Переменные параметры"""
-        return {}
-
-    def validate_substance(self, substance: Substance) -> None:
-        """Проверка параметров рабочего тела на входе"""
-        assert isinstance(substance, Substance), TypeError("type substance must be Substance")
-        assert substance.parameters.get(gtep.mf) is not None, AttributeError(SUBSTANCE_ATTRIBUTE_ERROR.format(substance.name, gtep.m))
-        assert substance.parameters.get(gtep.TT) is not None, AttributeError(SUBSTANCE_ATTRIBUTE_ERROR.format(substance.name, gtep.TT))
-        assert substance.parameters.get(gtep.PP) is not None, AttributeError(SUBSTANCE_ATTRIBUTE_ERROR.format(substance.name, gtep.PP))
+    def validate_substance(cls, substance: Substance) -> None:
+        """Проверка обязательных параметров рабочего тела"""
+        assert isinstance(substance, Substance), TypeError(f"type substance must be {Substance}")
+        # validate parameters
+        assert gtep.mf in substance.parameters, AttributeError(SUBSTANCE_ATTRIBUTE_ERROR.format(substance.name, gtep.m))
+        assert gtep.TT in substance.parameters, AttributeError(SUBSTANCE_ATTRIBUTE_ERROR.format(substance.name, gtep.TT))
+        assert gtep.PP in substance.parameters, AttributeError(SUBSTANCE_ATTRIBUTE_ERROR.format(substance.name, gtep.PP))
         # validate functions
         tdp_keys = tdp.values()  # разрешенный список термодинамических параметров
         for name, function in substance.functions.items():
@@ -99,50 +57,39 @@ class GTENode(ABC):
             for func_arg in function.__code__.co_varnames:
                 assert func_arg in tdp_keys, NameError(f"function '{name}' has arg '{func_arg}' not in {tdp_keys}")
 
-    def is_solvable(self, *args) -> str:
-        """Проверка резрешимости математической модели"""
-        try:
-            prediction = self.predict(*args, use_ml=False)  # для быстроты
-        except Exception as e:
-            return f"{e}"
+    @classmethod
+    def calculate_substance(cls, substance: Substance) -> Substance:
+        """Расчет термодинамических параметров вещества по массе, температуре, давлению"""
+        cls.validate_substance(substance)
 
-        count_outlet_variables = sum(1 if k.startswith("outlet") else 0 for k in prediction)  # количество неизвестных выходных параметров
-        args = {k: v for k, v in self.variables.items() if not isnan(v)}  # известные параметры узла
-        count_variables = len(self.variables) - len(args)  # количество неизвестных параметров узла
-        count_equations = len(self._equations(tuple(), args))  # количество уравнений
+        parameters = {tdp.t: substance.parameters.get(gtep.TT), tdp.p: substance.parameters.get(gtep.PP), tdp.eo: substance.parameters.get(gtep.eo)}
+        substance.parameters[gtep.gc] = call_with_kwargs(substance.functions[gtep.gc], parameters)
+        substance.parameters[gtep.Cp] = call_with_kwargs(substance.functions[gtep.Cp], parameters)
+        substance.parameters[gtep.DD] = substance.parameters[gtep.PP] / (substance.parameters[gtep.gc] * substance.parameters[gtep.TT])
+        substance.parameters[gtep.k] = adiabatic_index(substance.parameters[gtep.gc], substance.parameters[gtep.Cp])
+        substance.parameters[gtep.a_critical] = сritical_sonic_velocity(substance.parameters[gtep.k], substance.parameters[gtep.gc], substance.parameters[gtep.TT])
 
-        if count_variables + count_outlet_variables < count_equations:
-            return f"{count_variables=} + {count_outlet_variables=} < {count_equations=}"
-        elif count_variables + count_outlet_variables > count_equations:
-            return f"{count_variables=} + {count_outlet_variables=} > {count_equations=}"
-        else:
-            return ""
+        return substance
 
+    @classmethod
     @abstractmethod
-    def predict(self, inlet: Substance, use_ml: bool = True) -> Dict[str, float]:
+    def predict(cls, inlet: Substance, use_ml: bool = True) -> Dict[str, float]:
         """Начальные приближения"""
-        self.validate_substance(inlet)
+        cls.validate_substance(inlet)
+
         assert isinstance(use_ml, bool), TypeError(f"{type(use_ml)=} must be bool")
+
         return {}
 
-    @property
-    def summary(self) -> Dict[str, float]:
-        result = {}
-        for k in self.__slots__:
-            if not isinstance(getattr(self, k), Substance):
-                result[k] = getattr(self, k)
-            else:
-                for parameter, value in getattr(self, k).parameters.items():
-                    result[f"{k}_{parameter}"] = value
-        return result
-
-    def _equations(self, x: Tuple[float], args: Dict[str, Any]) -> Tuple:
-        """Уравнения"""
+    @classmethod
+    def _equations(cls, x: Tuple[float], args: Dict[str, Any]) -> Tuple:
+        """Система уравнений"""
         return tuple()
 
+    @classmethod
     @abstractmethod
-    def solve(self, x0: Dict = None) -> Substance:
-        """Расчет узла"""
+    def calculate(cls, x0: Dict[str, float] = None) -> Substance:
+        """Термодинамический расчет узла по СНЛАУ _equations"""
         # валидация входных параметров
         # расчет входных параметров
         # расчет параметров узла
@@ -150,12 +97,11 @@ class GTENode(ABC):
         # вывод выходных параметров
         pass
 
-    @abstractmethod
-    def is_real(self) -> bool:
-        """Проверка физичной реальности"""
-        return False
 
-    def reset(self) -> None:
-        """Сброс параметров узла"""
-        for k in self.variables:
-            setattr(self, k, nan)
+class TurboCompressor(ABC):
+    """Абстрактный класс турбокомрпессора"""
+
+    @abstractmethod
+    def get_power(self, inlet: Substance, rotation_frequency: float) -> float:
+        """Расчет мощности по частоте вращения и характеристике узла"""
+        return 0
