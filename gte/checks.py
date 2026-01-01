14af6b993f5cf6929a73cb1baf77d1dfd171b9b6
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2026-01-01 20:00:23 +0300

    add check_pressure

--- gte/checks.py
+++ gte/checks.py
@@ -1,6 +1,6 @@
-def check_efficiency(efficiency: float) -> bool:
-    """Проверка значения КПД"""
-    return 0 <= efficiency <= 1
+def check_mass_flow(mass_flow: float) -> bool:
+    """Проверка на отрицательный массовый расход"""
+    return 0 < mass_flow
 
 
 def check_temperature(temperature: float) -> bool:
@@ -8,6 +8,21 @@ def check_temperature(temperature: float) -> bool:
     return 0 <= temperature
 
 
+def check_pressure(pressure: float) -> bool:
+    """Проверка на отрицательное давление"""
+    return 0 <= pressure
+
+
+def check_efficiency(efficiency: float) -> bool:
+    """Проверка значения КПД"""
+    return 0 <= efficiency <= 1
+
+
 def check_pressure_ratio(pressure_ratio: float) -> bool:
     """Проверка степени повышения/понижения давления"""
     return 1 <= pressure_ratio
+
+
+def check_power(power: float) -> bool:
+    """Проверка на отрицательную мощность"""
+    return 0 <= power
