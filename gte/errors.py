a5db2420494c6729c4537e809c62ca2d70bb4e9f
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2026-02-17 00:58:19 +0300

    add errors

---
+++ gte/errors.py
@@ -0,0 +1,8 @@
+# шаблоны типичных ошибок
+TYPE_ERROR: str = "{} must be {}"
+SUBSTANCE_ATTRIBUTE_ERROR: str = "'{}' has not attribute '{}'"
+ITERATION_LIMIT_ERROR: str = "Iteration limit in {}"
+
+# проверки
+NEGATIVE_TEMPERATURE_ERROR: str = "Absolute temperature '{}' < 0 K"
+EFFICIENCY_ERROR: str = "Efficiency {} not in [0..1]"
