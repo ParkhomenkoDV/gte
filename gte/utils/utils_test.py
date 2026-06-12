9f1a2902464485e2db33eb58e881d3ea5e9acaed
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2026-06-12 17:27:43 +0300

    add new bench

--- gte/utils/utils_test.py
+++ gte/utils/utils_test.py
@@ -6,6 +6,11 @@ import pytest
 try:
     from .utils import Function, Interpolator, integral_average, integrate
 except ImportError:
+    import os
+    import sys
+
+    sys.path.insert(0, os.getcwd())
+
     from gte.utils.utils import Function, Interpolator, integral_average, integrate
 
 
