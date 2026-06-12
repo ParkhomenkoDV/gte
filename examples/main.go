7d934cb8f385e61b0762489e8f64ec3be760c8f3
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2026-06-12 23:24:58 +0300

    add air

--- examples/main.go
+++ examples/main.go
@@ -2,32 +2,81 @@ package main // обязательно main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/ParkhomenkoDV/gte/gte"
 	"github.com/ParkhomenkoDV/gte/gte/nodes/burner"
 	"github.com/ParkhomenkoDV/gte/gte/nodes/channel"
 	"github.com/ParkhomenkoDV/gte/gte/nodes/turbocompressor/rotor"
+	"github.com/ParkhomenkoDV/gte/gte/utils"
 	"github.com/ParkhomenkoDV/substance"
 )
 
+const T0 = 273.15
+
 func main() {
-	inlet := substance.Substance{
-		Name: "air",
-		Parameters: substance.Parameters{
-			"m": 50,
+	air_gc := utils.Function{
+		Name: "gas const",
+		Function: func(ps substance.Parameters) substance.Parameter {
+			return 287.14
 		},
-		Functions: substance.Functions{},
+		Args: map[string]struct{}{},
 	}
-	fuel := substance.Substance{
-		Name: "kerosene",
+	air_hcp := utils.Function{
+		Name: "heat capacity at const pressure",
+		Function: func(ps substance.Parameters) substance.Parameter {
+			tt_1000 := ps["TT"] / 1000
+			coefs := [7]float64{0.2521923, -0.1186612, 0.3360775, -0.3073812, 0.1382207, -0.03090246, 0.002745383}
+			result := 0.0
+			for i, c := range coefs {
+				result += c * math.Pow(tt_1000, float64(i))
+			}
+			return 4187.0 * result
+		},
+		Args: map[string]struct{}{"TT": {}},
+	}
+
+	air := substance.Substance{
+		Name: "air",
+		Composition: map[string]float64{
+			"N2": 0.78, "O2": 0.21, "Ar": 0.009, "CO2": 0.0004,
+		},
 		Parameters: substance.Parameters{
-			"m": 1,
+			"m":   50,
+			"gc":  287.14,
+			"TT":  300.0,
+			"PP":  101325.0,
+			"hcp": 1006.0,
+			"k":   1.4,
+			"c":   0.0,
+		},
+		Functions: substance.Functions{
+			"gc": func(ps substance.Parameters) substance.Parameter {
+				return air_gc.Call(ps)
+			},
+			"hcp": func(ps substance.Parameters) substance.Parameter {
+				return air_hcp.Call(ps)
+			},
+		},
+	}
+
+	kerosene := substance.Substance{
+		Name: "kerosene",
+		Composition: map[string]float64{
+			"C": 0.85, "H": 0.15,
+		},
+		Parameters: substance.Parameters{
+			"m":             1,
+			"TT":            40 + T0,
+			"PP":            101_325,
+			"stoichiometry": 14.61,
+			"lower_heat":    43_000_000,
 		},
 		Functions: substance.Functions{},
 	}
 
-	fmt.Printf("inlet: %+v \n", inlet)
-	fmt.Printf("fuel: %+v \n", fuel)
+	fmt.Printf("inlet: %+v \n", air)
+	fmt.Printf("fuel: %+v \n", kerosene)
 
 	c := rotor.Rotor{
 		Name:       "HPC",
